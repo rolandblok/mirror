6aacc0733779fbb2b54df1f5e910bed55e9d9268
roland <rolandblok@gmail.com>
2022-02-08 17:00:29 +0100

    realsense

---
+++ realsense_tracking/opencv_viewer_example.py
@@ -0,0 +1,103 @@
+## License: Apache 2.0. See LICENSE file in root directory.
+## Copyright(c) 2015-2017 Intel Corporation. All Rights Reserved.
+
+###############################################
+##      Open CV and Numpy integration        ##
+###############################################
+
+import time, math
+import pyrealsense2 as rs
+import numpy as np
+import cv2
+print("opencv version : " + cv2.__version__ )
+
+# Configure depth and color streams
+pipeline = rs.pipeline()
+config = rs.config()
+
+# Get device product line for setting a supporting resolution
+pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+pipeline_profile = config.resolve(pipeline_wrapper)
+device = pipeline_profile.get_device()
+device_product_line = str(device.get_info(rs.camera_info.product_line))
+
+found_rgb = False
+for s in device.sensors:
+    if s.get_info(rs.camera_info.name) == 'RGB Camera':
+        found_rgb = True
+        break
+if not found_rgb:
+    print("The demo requires Depth camera with Color sensor")
+    exit(0)
+
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+
+if device_product_line == 'L500':
+    config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
+else:
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+# Start streaming
+pipeline.start(config)
+
+
+# https://docs.opencv.org/4.5.5/d4/d26/samples_2cpp_2facedetect_8cpp-example.html
+# https://opencv-tutorial.readthedocs.io/en/latest/face/face.html
+# https://www.geeksforgeeks.org/face-detection-using-cascade-classifier-using-opencv-python/
+
+
+try:
+    while True:
+
+        # Wait for a coherent pair of frames: depth and color
+        frames = pipeline.wait_for_frames()
+        depth_frame = frames.get_depth_frame()
+        color_frame = frames.get_color_frame()
+        if not depth_frame or not color_frame:
+            continue
+
+        # Convert images to numpy arrays
+        depth_image = np.asanyarray(depth_frame.get_data())
+        color_image = np.asanyarray(color_frame.get_data())
+
+        # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+
+        depth_colormap_dim = depth_colormap.shape
+        color_colormap_dim = color_image.shape
+
+        # If depth and color resolutions are different, resize color image to match depth image for display
+        if depth_colormap_dim != color_colormap_dim:
+            resized_color_image = cv2.resize(color_image, dsize=(depth_colormap_dim[1], depth_colormap_dim[0]), interpolation=cv2.INTER_AREA)
+            images = np.hstack((resized_color_image, depth_colormap))
+        else:
+            images = np.hstack((color_image, depth_colormap))
+
+        gray_image = cv2.cvtColor( color_image, cv2.COLOR_BGR2GRAY )
+        haar_cascade = cv2.CascadeClassifier('xml/haarcascade_frontalface_default.xml')
+        faces_rect = haar_cascade.detectMultiScale( gray_image, scaleFactor=1.1, minNeighbors=9)
+        
+        for (x, y, w, h) in faces_rect:
+            cv2.rectangle(images, (x, y), (x+w, y+h), (0, 255, 0), thickness=2)
+            xm = math.floor(x + w/2)
+            ym = math.floor(y + h/2)
+            dist = depth_frame.get_distance(xm, ym)
+            cv2.putText(images, "{:.3f}".format(dist), (xm,ym), cv2.FONT_HERSHEY_SIMPLEX , 2, (255,255,255), thickness=2 )
+  
+
+        # Show images
+        cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+        cv2.imshow('RealSense', images)
+        key = cv2.waitKey(1)
+        if(key == ord('x')):
+            print("quiting")
+            break
+
+finally:
+    print("Stop streaming")
+    cv2.destroyAllWindows()
+    time.sleep(1)
+    exit(0)
+    pipeline.stop()
+    quit()
+
