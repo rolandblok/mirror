68bd2976c1d94a18de822a266772f0932657bdd8
rolandblok <rolandblok@gmail.com>
2022-02-09 09:42:09 +0100

    openvino

---
+++ realsense_tracking/openvino/face_recog_openvino.bat
@@ -0,0 +1,3 @@
+@REM locatie spullen:C:\Program Files (x86)\Intel\openvino_2021.4.752\deployment_tools\open_model_zoo\demos\face_recognition_demo\python>
+
+python .\face_recognition_demo.py -i 1 -m_fd "xml\face-detection-retail-0004.xml" -m_lm "xml\landmarks-regression-retail-0009.xml" -m_reid "xml\face-reidentification-retail-0095.xml"
