bb153e345387e2d3f3a9d3af6fc279523e012251
roland <rolandblok@gmail.com>
2022-02-09 17:34:54 +0100

    mediapip

---
+++ realsense_tracking/mediapipe/face_detect.py
@@ -0,0 +1,97 @@
+import cv2
+import mediapipe as mp
+mp_face_detection = mp.solutions.face_detection
+mp_drawing = mp.solutions.drawing_utils
+
+# # For static images:
+# IMAGE_FILES = []
+# with mp_face_detection.FaceDetection(
+#     model_selection=1, min_detection_confidence=0.5) as face_detection:
+#   for idx, file in enumerate(IMAGE_FILES):
+#     image = cv2.imread(file)
+#     # Convert the BGR image to RGB and process it with MediaPipe Face Detection.
+#     results = face_detection.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
+#     # Draw face detections of each face.
+#     if not results.detections:
+#       continue
+#     annotated_image = image.copy()
+#     for detection in results.detections:
+#       print('Nose tip:')
+#       print(mp_face_detection.get_key_point(
+#           detection, mp_face_detection.FaceKeyPoint.NOSE_TIP))
+#       mp_drawing.draw_detection(annotated_image, detection)
+#     cv2.imwrite('/tmp/annotated_image' + str(idx) + '.png', annotated_image)
+
+# For webcam input:
+# cap = cv2.VideoCapture(1)
+
+import pyrealsense2 as rs
+import numpy as np
+import time, math
+
+# Configure depth and color streams
+pipeline = rs.pipeline()
+config = rs.config()
+
+# Get device product line for setting a supporting resolution
+pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+pipeline_profile = config.resolve(pipeline_wrapper)
+device = pipeline_profile.get_device()
+device_product_line = str(device.get_info(rs.camera_info.product_line))
+print(device_product_line)
+
+found_rgb = False
+for s in device.sensors:
+    if s.get_info(rs.camera_info.name) == 'RGB Camera':
+        found_rgb = True
+        break
+if not found_rgb:
+    print("The demo requires Depth camera with Color sensor")
+    exit(0)
+
+config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+# config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 6)
+# Start streaming
+pipeline.start(config)
+
+
+with mp_face_detection.FaceDetection(
+    model_selection=0, min_detection_confidence=0.5) as face_detection:
+#   while cap.isOpened():
+    while True:
+        frames = pipeline.wait_for_frames()
+        color_frame = frames.get_color_frame()
+        # success, image = cap.read()
+        # if not success:
+        #     print("Ignoring empty camera frame.")
+        #     # If loading a video, use 'break' instead of 'continue'.
+        #     continue.
+        if not color_frame:
+            continue
+        # Convert images to numpy arrays
+        image = np.asanyarray(color_frame.get_data())
+
+        # To improve performance, optionally mark the image as not writeable to
+        # pass by reference.
+        image.flags.writeable = False
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = face_detection.process(image)
+
+        # Draw the face detection annotations on the image.
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        if results.detections:
+            for detection in results.detections:
+                mp_drawing.draw_detection(image, detection)
+            # Flip the image horizontally for a selfie-view display.
+        cv2.imshow('MediaPipe Face Detection', cv2.flip(image, 1))
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+
+print("Stop streaming")
+cv2.destroyAllWindows()
+time.sleep(1)
+exit(0)
+pipeline.stop()
+quit()
