b4756558604815fb1f5c1d8cd929eb2c5ac49ee2
roland <rolandblok@gmail.com>
2022-06-16 16:25:38 +0200

    web control and crontab

---
+++ start_mirror.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+cd /home/pi/mirror/realsense_tracking
+
+for (( ; ; ))
+do
+   echo "start narcissus"  >> /home/pi/mirror.output.txt 2>&1
+   date                    >> /home/pi/mirror.output.txt 2>&1
+   python narcissus.py noterm noscreen      >> /home/pi/mirror.output.txt 2>&1
+   if test -f "/home/pi/mirror_stop"; then
+	echo "mirror stops due to file"  >> /home/pi/mirror.output.txt 2>&1
+	exit 0
+   fi
+done
+
+# python narcissus.py
+
+
+
+
+
