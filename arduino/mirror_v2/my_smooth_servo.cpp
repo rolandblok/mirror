0d54ef93a7c8d1a6b24353c9a104865ddd6fd0f3
roland <rolandblok@gmail.com>
2022-07-04 10:49:50 +0200

    stepper

--- arduino/mirror_v2/my_smooth_servo.cpp
+++ arduino/mirror_v2/my_smooth_servo.cpp
@@ -4,11 +4,11 @@
 
 //https://rootsaid.com/pca9685-servo-driver/
 
-#define SERVOMIN  (150) // This is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX  (600) // This is the 'maximum' pulse length count (out of 4096)
+#define SERVOMIN  (2*150) // This is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  (2*600) // This is the 'maximum' pulse length count (out of 4096)
 #define USMIN  (600) // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
 #define USMAX  (2400) // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
-#define SERVO_FREQ (50) // Analog servos run at ~50 Hz updates
+#define SERVO_FREQ (2*50) // Analog servos run at ~50 Hz updates
 
 #define SERVO_ANGLE_MIN  (-40)
 #define SERVO_ANGLE_MAX  ( 40)
